a4e842e3640d76777df7fca52129166389e13b9f
Linus Koepfer <linkoep@gmail.com>
2022-10-29 21:42:04 -0400

    Finish up

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const phrases = [
   ["I Approve!"],
   ["Good Stuff!"],
   ["Yes!"],
-  ["Looks Good To Me", ".12px"],
-  ["Correct!"]
+  ["Looks Good To Me!", ".12px"],
+  ["Correct!"],
+  ["Nice Work!"],
+  ["You Got It!"],
 ];
 
-let t = phrases[Math.floor(Math.random() * phrases.length)];
-document.getElementById("seal_speech").textContent = t[0];
-if (t.length > 1) {
-  document.getElementById("seal_speech").style.fontsize=text[1]
+function genText() {
+  let t = phrases[Math.floor(Math.random() * phrases.length)];
+  document.getElementById("seal_speech").textContent = t[0];
+  if (t.length > 1) {
+    document.getElementById("seal_speech").style.fontSize=t[1];
+  }
 }
+
+document.getElementById("another").onclick = genText
+
+genText()
